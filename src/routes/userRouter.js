b7e9e91249a6d92b508d27dcb0dceed6554c11e2
wonkooklee <const.wonkook@gmail.com>
2021-10-12 02:04:14 +0900

    Removed: 테스트용 라우트 핸들러 제거

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,13 +3,6 @@ import { userController } from '../controllers';
 
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  res.json({
-    status: 'success',
-    message: 'hi',
-  });
-});
-
 router.route('/login').post(userController.login);
 router.route('/signup').post(userController.signUp);
 router.route('/id').post(userController.findUser);
